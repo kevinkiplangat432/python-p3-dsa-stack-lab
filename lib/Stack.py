b4ccca9012c2572d30c99da0fd42f7f28d2175d5
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-12-04 07:35:51 +0300

    initialcommit

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,35 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items =None, limit = 100):
+        self.items = items if items is not None else []
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if self.full():
+            return 
+        self.items.append(item)
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.items.pop()
 
     def peek(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return self.size() >= self.limit
 
     def search(self, target):
-        pass
+        for i in range(len(self.items)-1, -1, -1):
+            if self.items[i] == target:
+                return (len(self.items)-1) - i
+        return -1
